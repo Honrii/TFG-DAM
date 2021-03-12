0d48420be02351a8f18444ec7a994f6af44f95b1
Dani Honrado <65502812+Honrii@users.noreply.github.com>
2021-03-12 16:31:30 +0100

    PrototipoFallido

--- php/PaginaPrincipal/index.php
+++ php/PaginaPrincipal/index.php
@@ -28,16 +28,14 @@
                 <i class="fa fa-search" aria-hidden="true"></i><input type="text" id="t1" placeholder="Origen:">
                 <!-- API Google Maps Buscador https://www.youtube.com/watch?v=43-OclfQ68I-->
                 <input type="text" id="t2" placeholder="Destino:">
-                <input type="date" id="t3" placeholder="Ida:">
-                <input type="date" id="t4" placeholder="Vuelta:">
+                <input type="date" id="t3" placeholder="Ida:" id="start" name="trip-start" value="2021-03-11"
+                    min="2021-03-11" max="2023-03-11">
+                <input type="date" id="t4" placeholder="Vuelta:" id="finish" name="trip-finish" value="2021-03-12"
+                    min="2021-03-11" max="2023-03-11">
                 <input type="number" id="t4" placeholder="Personas:">
                 <!-- TODO: Hacer dropdown para saber el numero de adultos y niños que hay -->
                 <input type="submit" value="Buscar">
             </form>
-            <div id="empresas">
-                <h3>Comparamos varias webs de reserva al mismo tiempo</h3>
-                <ul class="dealform-partners__wrapper"><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/626.png" alt="Booking.com"><span class="visuallyhidden">Booking.com</span></li><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/452_1.png" alt="Hotels.com"><span class="visuallyhidden">Hotels.com</span></li><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/395.png" alt="Agoda"><span class="visuallyhidden">Agoda</span></li><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/2420_1.png" alt="VRBO"><span class="visuallyhidden">VRBO</span></li><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/14.png" alt="Accor"><span class="visuallyhidden">Accor</span></li><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/634.png" alt="Trip.com"><span class="visuallyhidden">Trip.com</span></li><li class="partner"><img class="partner__logo" aria-hidden="true" src="https://imgcy.trivago.com/image/upload/hardcodedimages/mpm-localised-logos/582.png" alt="Viajes El Corte Ingles"><span class="visuallyhidden">Viajes El Corte Ingles</span></li><li class="partner"><p class="dealform-partners__text">Más de 100</p></li></ul>
-            </div>
         </div>
     </section>
 
