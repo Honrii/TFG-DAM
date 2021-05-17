51f7367202066974f41857e905695f769cb82412
HONRADO\usuario <dhonradoma@gmail.com>
2021-05-17 17:54:07 +0200

    1st try check user

--- php/loginRegister/insert.php
+++ php/loginRegister/insert.php
@@ -8,16 +8,29 @@ if (isset($_POST['regist'])) {
     $mail = $_POST['mail'];
     $password = $_POST['pwd'];
 
-    $sql = $conn->prepare("INSERT INTO usuarios (name, correo, pwd)VALUES ( ?, ?, ?)");
+  /*  $sql = $conn->prepare("INSERT INTO usuarios (name, correo, pwd)VALUES ( ?, ?, ?)");
     $sql->bind_param("sss", $nom, $mail, $password);
-    // $result = $conn->query($sql);
+    
 
-    $sql->execute();
-    // if ($result)
-    //     echo "<br> Se ha ejectudado el delete correctamente";
+    $sql->execute();*/
+  
 
-    // else
-    //     echo "<br> ALGO NO FUNCIONA";
-   //          closeConn($conn);
-    //    //   header("Location: index.php");
+
+    /*Check new user*/
+    $sql_u = "SELECT * FROM users WHERE name='$nom'";
+  	$sql_e = "SELECT * FROM users WHERE correo='$mail'";
+  	$res_u = mysqli_query($sql_u);
+  	$res_e = mysqli_query($sql_e);
+
+  	if (mysqli_num_rows($res_u) > 0) {
+  	  $name_error = "Sorry... username already taken"; 	
+  	}else if(mysqli_num_rows($res_e) > 0){
+  	  $email_error = "Sorry... email already taken"; 	
+  	}else{
+        $sql = $conn->prepare("INSERT INTO usuarios (name, correo, pwd)VALUES ( ?, ?, ?)");
+        $sql->bind_param("sss", $nom, $mail, $password);
+        
+        $sql->execute();
+  	}
+    
 }
