2c57adda993f73540e1d0031c3a8c09fd5f6abd5
HONRADO\usuario <dhonradoma@gmail.com>
2021-05-26 12:21:13 +0200

    db heroku2

--- php/connDB.php
+++ php/connDB.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "localhost";
+$servername = "eu-cdbr-west-01.cleardb.com";//localhost
 $username = "ba8f0037208220";// root
 $password = "da99f8f4";//da99f8f4
 $dbname = "heroku_83c1bab7b4bbea2";//heroku_83c1bab7b4bbea2
