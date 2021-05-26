973e550866292a44cf233052a7fd25c9623bada6
HONRADO\usuario <dhonradoma@gmail.com>
2021-05-26 12:01:04 +0200

    change db

--- php/connDB.php
+++ php/connDB.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "users";
+$username = "ba8f0037208220";// root
+$password = "da99f8f4";//da99f8f4
+$dbname = "heroku_83c1bab7b4bbea2";//heroku_83c1bab7b4bbea2
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
